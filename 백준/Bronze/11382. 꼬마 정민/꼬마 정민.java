c594dbb34856b074321019de665a97d1f9712a53
JUNG DONGOH <112747309+JUNG-DONGOH@users.noreply.github.com>
2025-09-25 00:38:37 +0900

    Update 꼬마 정민.java

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		// 자료형이 int는 범위를 넘기때문에 Long을 써줌
 		try {
 			long A = scanner.nextLong();
 			long B = scanner.nextLong();
@@ -16,3 +17,4 @@ public class Main {
 		}
 	}
 }
+
