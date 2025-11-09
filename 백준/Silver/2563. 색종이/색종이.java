5ce8186a2024cda023ccf0d7fd606762ffbe6c92
JUNG DONGOH <112747309+JUNG-DONGOH@users.noreply.github.com>
2025-11-10 00:00:26 +0900

    Update 색종이.java

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class Main {
+public class Lv2563 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean[][] area = new boolean[100][100];
+        boolean[][] area = new boolean[100][100]; //2차원 배열로 구성
         int paperNum = scanner.nextInt();
 
-        for (int i = 0; i < paperNum; i++) {
+        for (int i = 0; i < paperNum; i++) { // paperNum만큼 반복
             int xStart = scanner.nextInt();
             int yStart = scanner.nextInt();
 
             for (int x = xStart; x < xStart + 10; x++) {
                 for (int y = yStart; y < yStart + 10; y++) {
-                    area[x][y] = true;
+                    area[x][y] = true; // area[x][y]부분에 false->true로 바꿈
                 }
             }
         }
@@ -20,7 +20,7 @@ public class Main {
         int count = 0;
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
-                if (area[i][j]) count++;
+                if (area[i][j]) count++; // area[x][y]부분이 true면 count 증가
             }
         }
 
